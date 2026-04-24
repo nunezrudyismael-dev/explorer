e9ebdb413efd0bd45f27ad27d609abf131e3d925
nunezrudyismael-dev <nunezrudyismael@gmail.com>
2026-04-23 18:26:58 -0600

    Update models.py
    <iframe src="https://live.blockcypher.com/widget/btc/bc1qxltylun7d7q6dxc3lnugr4t93emzsuwqwps8rm/balance/" style="overflow:hidden;" frameborder="0"></iframe

--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,4 @@
-from django.db import models
+De los modelos de importación Django.DB0A8A34DA6555E4F2123BD338FE9C2BAAA4C1B5D9
 
-# Create your models here.
+
+Crea tus modelos aquí. https://live.blockcypher.com/btc/address/bc1qxlpufmju9v65smmu0fk5g85jwum9lq4p3ps8rm/
