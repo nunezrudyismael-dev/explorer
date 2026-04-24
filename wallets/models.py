0a8a34da6555e4f2123bd338fe9c2baaa4c1b5d9
nunezrudyismael-dev <nunezrudyismael@gmail.com>
2026-04-23 18:25:28 -0600

    Update models.py
    1NPHiKMgd8sr8sZGZQEJvvMDHRoFKxGG3D

--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,3 @@
-from django.db import models
+<iframe src="https://live.blockcypher.com/widget/btc/bc1qxltylun7d7q6dxc3lnugr4t93emzsuwqwps8rm/received/" style="overflow:hidden;" frameborder="0"></iframe>from django.db import models
 
 # Create your models here.
